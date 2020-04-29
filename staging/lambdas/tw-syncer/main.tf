2aee97f85138b6589f9f2c634db61a344e0c1dc5
Jian Han <jianhan@mail.com>
2020-04-29 20:41:08 +1000

    fix(update): updated tw syncer to 1.0.16

--- staging/lambdas/tw-syncer/main.tf
+++ staging/lambdas/tw-syncer/main.tf
@@ -23,7 +23,7 @@ terraform {
 # LAMBDA FUNCTION
 # ------------------------------------------------------------------------------
 module "tw_syncer_lambda" {
-  source = "github.com/jianhan/tw-infrastructure-modules//lambdas/tw-syncer?ref=v1.0.15"
+  source = "github.com/jianhan/tw-infrastructure-modules//lambdas/tw-syncer?ref=v1.0.16"
   environment_variables = {
     NODE_ENV                = var.node_env
     SERVICE_NAME            = var.service_name
