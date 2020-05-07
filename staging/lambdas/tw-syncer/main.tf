5e83a8ea712ee119e1f23ada37a82e5ef17805e0
Jian Han <jianhan@mail.com>
2020-05-07 17:26:58 +1000

    feat(update): update lambda version to be 1.31.2

--- staging/lambdas/tw-syncer/main.tf
+++ staging/lambdas/tw-syncer/main.tf
@@ -36,5 +36,5 @@ module "tw_syncer_lambda" {
     S3_BUCKET_NAME          = var.s3_bucket_name
   }
   lambda_function_s3_bucket = "jian-personal-lambdas"
-  lambda_function_s3_key    = "tw-syncer/v1.31.1/lambda-v1.31.1.zip"
+  lambda_function_s3_key    = "tw-syncer/v1.31.2/lambda-v1.31.2.zip"
 }
