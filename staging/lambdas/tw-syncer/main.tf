86f9c226e7188da9c06de5463876496f1531374a
Jian Han <jianhan@mail.com>
2020-05-08 12:25:31 +1000

    fix(typo): updated version to be 1.1.2 to fix typo.

--- staging/lambdas/tw-syncer/main.tf
+++ staging/lambdas/tw-syncer/main.tf
@@ -23,7 +23,7 @@ terraform {
 # LAMBDA FUNCTION
 # ------------------------------------------------------------------------------
 module "tw_syncer_lambda" {
-  source = "github.com/jianhan/tw-infrastructure-modules//lambdas/tw-syncer?ref=v1.1.1"
+  source = "github.com/jianhan/tw-infrastructure-modules//lambdas/tw-syncer?ref=v1.1.2"
   environment_variables = {
     NODE_ENV                = var.node_env
     SERVICE_NAME            = var.service_name
